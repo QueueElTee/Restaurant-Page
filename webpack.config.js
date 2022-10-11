ecbdea0707e892d82f1373ca06eb864fb2dd252e
QueueElTee <tijaniqueen17@gmail.com>
2022-10-11 14:26:37 +0100

    Revert "Add Favicon To Source Folder"
    This reverts commit f7d651415b1ba5f1d4e78a3489c6c2f840d21eaa.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +8,8 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new FaviconsWebpackPlugin('./src/apple-touch-icon.png'),
+        new HtmlWebpackPlugin(),
+    ],
 };
