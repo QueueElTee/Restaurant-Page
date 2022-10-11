900e2736b956eec11d69c0ce48f24c31777172f7
QueueElTee <tijaniqueen17@gmail.com>
2022-10-11 14:30:15 +0100

    Remove Plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,8 +6,4 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-        new FaviconsWebpackPlugin('./src/apple-touch-icon.png'),
-        new HtmlWebpackPlugin(),
-    ],
 };
